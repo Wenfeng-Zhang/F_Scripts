4c03781069f08e0163ba55eea1427ef7abe926a5
Wenfeng-Zhang <zwf.vfx@foxmail.com>
2019-10-25 23:56:58 +0800

    快捷切换merge节点里的叠加方式

--- merge_short/merge_short.py
+++ merge_short/merge_short.py
@@ -3,9 +3,10 @@
 #https://github.com/Wenfeng-Zhang
 
 '''
+import merge_short
 toolbar = nuke.menu('Nodes')
 m = toolbar.addMenu('F Script')
-m.addCommand('Merge shot', 'Merge_Short()', 'alt+o')
+m.addCommand('Merge shot', 'merge_short.Merge_Short()', 'alt+o')
 '''
 
 import nuke
